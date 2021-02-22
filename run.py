016a92d5bb457c15b1f09cdc6d2b24d5067ea1d1
Sudeep <sudeep.dalai.5417@gmail.com>
2021-02-22 14:59:03 +0530

    Made changes according to errors on Heroku App

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 from app import app
 from db import db
 
-db.init(app)
+db.init_app(app)
 
 
 # this function runs before any http request is made - used for creation of rss. like tables
 @app.before_first_request
 def create_table():
-    db.create_all()     # creates all necessary tables automatically
+    db.create_all()  # creates all necessary tables automatically
